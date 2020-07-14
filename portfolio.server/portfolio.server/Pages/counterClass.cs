5b6fb7a8c75154a90f2dc5134c7b30ca2aef0e9b
polsevev <rolfmg@VisaviTestmaskin.localdomain>
2020-07-14 15:31:47 +0200

    Next is getting the API to work :)

---
+++ portfolio.server/portfolio.server/Pages/counterClass.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portfolio.server.Pages
+{
+    public class counterClass: ComponentBase
+    {
+        public int currentCount = 0;
+        public void incrementCount()
+        {
+            currentCount += 50000;
+        }
+    }
+}
